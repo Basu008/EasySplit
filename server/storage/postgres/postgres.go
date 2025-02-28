59cefa5ea243c0b4fdacd720d1c0a8bac674d4b4
Basu Singh <basu.singh@hypd.in>
2025-03-01 02:20:30 +0530

    removed debug statements

--- server/storage/postgres/postgres.go
+++ server/storage/postgres/postgres.go
@@ -1,7 +1,6 @@
 package postgresStorage
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/Basu008/EasySplit.git/server/config"
@@ -16,7 +15,6 @@ type PostgresStorage struct {
 
 func NewPostgresStorage(c *config.DatabaseConfig) *PostgresStorage {
 	dsn := c.ConnectionURL()
-	fmt.Println(dsn)
 	var db *gorm.DB
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -24,6 +22,5 @@ func NewPostgresStorage(c *config.DatabaseConfig) *PostgresStorage {
 		log.Fatalf("failed to establish connection with postgres: %s", err.Error())
 		return nil
 	}
-	fmt.Printf("\nDB instance: %+v", db)
 	return &PostgresStorage{Config: c, DB: db}
 }
